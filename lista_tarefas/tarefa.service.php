5e49b785b9d610647f670316116fea35804392dd
Rioki01 <134452025+Rioki01@users.noreply.github.com>
2024-03-12 20:26:25 -0300

    -commit atualizar
    aula 12/03/2024

--- lista_tarefas/tarefa.service.php
+++ lista_tarefas/tarefa.service.php
@@ -1,6 +1,7 @@
 <?php
 //lida com consultas de dados.
-class TarefeaService{
+//Metodos CRUD
+class TarefaService{
     private $conexao;
     private $tarefa;
 
@@ -10,8 +11,11 @@ class TarefeaService{
         $this->tarefa = $tarefa;
     }
 
+    //CRUDE
     public function inserir(){
         //C - Create
+        //cria uma query, e joga no banco de dados, preparando ela para ser executada em uma conexao.!
+        //campos com "?" ou ":" não sao jogados e deve ser inseridos manualmente com bindValue.
         $query = 'insert into tb_tarefas(tarefa)values(:tarefa)';
         //joga no banco os valores, utilizando o metodo prepare.
         $stmt = $this->conexao->prepare($query);
@@ -31,6 +35,51 @@ class TarefeaService{
         ';
         $stmt = $this->conexao->prepare($query);
         $stmt->execute();
+        //fecha a conexao, somente em SELECT
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function atualizar(){
+        //U - Update
+        //valores para atualizar devem ser em ?, e para atualizar duplas, é necessaria um ID.
+        //chama e numera a função bindValue.
+        $query = "update tb_tarefas set tarefa = ? where id = ?";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(1, $this->tarefa->__get('tarefa'));
+        $stmt->bindValue(2, $this->tarefa->__get('id'));
+        //executa
+        return $stmt->execute();
+    }
+
+    public function remover(){
+        //D - Delete
+        $query = 'delete from tb_tarefas where id = :id';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':id', $this->tarefa->__get('id'));
+        $stmt->execute();
+    }
+    public function marcarRealizada(){
+        //Igual a U - Update
+        //somente muda tarefa para id_status
+        //valores para atualizar devem ser em ?, e para atualizar duplas, é necessaria um ID.
+        //chama e numera a função bindValue.
+        $query = "update tb_tarefas set id_status = ? where id = ?";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(1, $this->tarefa->__get('id_status'));
+        $stmt->bindValue(2, $this->tarefa->__get('id'));
+        //executa
+        return $stmt->execute();
+    }
+    public function recuperarPendentes(){
+        $query = 'select t.id, s.status, t.tarefa
+        from tb_tarefas as t
+        left join tb_status as s on (t.id_status = s.id)
+        where 
+            t.id_status = :id_status
+        ';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':id_status', $this->tarefa->__get('id_status'));
+        $stmt->execute();
+        //fecha a conexao, somente em SELECT
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
--- lista_tarefas/tarefa_controller.php
+++ lista_tarefas/tarefa_controller.php
@@ -1,6 +1,76 @@
 <?php
+//Logica e instancias vem deste arquivo
+//require para puxar outros arquivos
+require "tarefa.model.php";
+require "tarefa.service.php";
+require "conexao.php";
+
+//variavel ação que ouve o usuario.
+//primeiro checa com isset se há alguma ação dentro dele.
+//isset é um teste logico, suas condiçoes -> apos o ? (quando for verdadeira), e apos o : (quando for falsa).
+//se ja tiver, ela continua a mesma, pois ja possui um valor.
+$acao = isset($_GET["acao"]) ? $_GET["acao"] : $acao;
+
+if ($acao == "inserir") { ////INSERIR
+    //cria instnacias de Tarefa e Construção para o metodo construtor da tarefa.service.
+    $tarefa = new Tarefa();
+    //pega com o set o valor do usuario.
+    //armazena o valor utilizando metodo global _post[]
+    $tarefa->__set("tarefa", $_POST['tarefa']);
+
+    $conexao = new Conexao();
+    //metodo construtor do tarefa.service
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    //chama a função de inserir do tarefa.service.
+    $tarefaService->inserir();
+
+    header('Location: (proximos passos)');
+} else if ($acao == 'recuperar') {  ////RECUPERAR
+    $tarefa = new Tarefa();
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    //Como a função recuperar tem Return, ela precisa de alguma variavel para recebeder os dados.
+    //neste caso "tarefas".
+    $tarefas = $tarefaService->recuperar();
+} else if ($acao == 'atualizar') { ////UPDATE
+    $tarefa = new Tarefa();
+    //seta os valores a serem atualizados.
+    $tarefa->__set('id', $_POST['id']);
+    $tarefa->__set('tarefa', $_POST['tarefa']);
+
+    $conexao = new Conexao();
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->atualizar();
+}  else if( $acao == 'remover') {  ////DELETAR
+
+    $tarefa = new Tarefa();
+    //pega o ID com GET para saber qual deletar.
+    $tarefa->__set('id', $_GET['id']);
+
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->remover();
+}   else if( $acao == 'recuperarPendentes') {   ////RECUPERAR PENDENTE
+    $tarefa = new Tarefa();
+    //pega o ID com GET para saber qual deletar.
+    $tarefa->__set('id_status', 0); // para pendentes
+    $conexao = new Conexao();
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefas = $tarefaService->recuperarPendentes();
+}   else if ( $acao == 'marcarRealizada') {     ////MARCAR REALIZADO
+    $tarefa = new Tarefa();
+    $conexao = new Conexao();
+    //pega o id com SET e GET e o seta o id_status para concluida!
+    $tarefa->__set('id', $_GET['id']->__set('id_status', 1));
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->marcarRealizada();
+    
 
 
-
+}
 
 ?>
